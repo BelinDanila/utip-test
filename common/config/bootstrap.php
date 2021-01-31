cb2359004e931e19ebaaec67d6cddfa87a8a93ff
Danila Belin <dbelin@enazadev.ru>
2021-01-31 21:40:41 +0500

    initial commit

---
+++ common/config/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+Yii::setAlias('@common', dirname(__DIR__));
+Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
+Yii::setAlias('@admin', dirname(dirname(__DIR__)) . '/admin');
+Yii::setAlias('@rest', dirname(dirname(__DIR__)) . '/rest');
+//Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
+Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
